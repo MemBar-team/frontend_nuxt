6021d00d60d606609890a32a1363526889bf43ff
Higemura <higemura.mustache@gmail.com>
2019-05-13 18:33:45 +0900

    不要コメント削除

--- middleware/authLogged.js
+++ middleware/authLogged.js
@@ -2,10 +2,6 @@ export default ({ store, redirect }) => {
   console.log('authLogged')
   console.log(store.state.authUser)
   if (store.state.authUser) {
-    // error({
-    //   message: 'You are not connected',
-    //   statusCode: 403
-    // })
     console.log('authLogged True')
     return redirect('/')
   }
