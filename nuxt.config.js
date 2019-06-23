c001889b4e2a8cf3b3b05626909024a887da3d37
Higemura <higemura.mustache@gmail.com>
2019-06-23 17:33:18 +0900

    不要コード削除 package.json 更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,13 +32,6 @@ export default {
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e14343' },
       { rel: 'stylesheet',  href: '//fonts.googleapis.com/icon?family=Material+Icons' }
     ]
-    // <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    // <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    // <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    // <link rel="manifest" href="/site.webmanifest">
-    // <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e14343">
-    // <meta name="msapplication-TileColor" content="#e14343">
-    // <meta name="theme-color" content="#ffffff"></meta>
   },
 
   /*
